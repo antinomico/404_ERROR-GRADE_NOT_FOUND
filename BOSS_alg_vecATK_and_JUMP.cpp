3c79977243d6c4c0408c452e567f04bf42bb2de4
mmcm2-cin <mmcm2@cin.ufpe.br>
2025-03-21 15:17:42 -0300

    Update BOSS_alg_vecATK_and_JUMP.cpp
    adicionei todos os cooldowns antes dos ataques e algo para monitorar isso

--- BOSS_alg_vecATK_and_JUMP.cpp
+++ BOSS_alg_vecATK_and_JUMP.cpp
@@ -8,6 +8,9 @@
 #define CENTER_x Hres/2
 #define CENTER_y Vres/2
 
+#define inicio_player_x 540
+#define inicio_player_y 630
+
 #define ATK_COOLDOWN_vec 1
 
 //-----------------------------------------------------------//
@@ -28,25 +31,33 @@ struct Sapo {
     coords posicao_inicial;
     coords posicao_do_pulo;
 
-    bool linguada = true;
+    bool linguada = false;
     bool pulando = false;
     bool porradao = false;
     bool reduzir_linguadas = true;
     bool tempo_antes_de_pular = false;
+    bool tempo_antes_do_porradao = false;
+    bool tempo_antes_da_linguada = true;
 
     int quantidade_de_linguadas = 5;
     int linguadas_restantes;
 
-    float cooldown_ataque = 0.1f;
-    float acompanhar_cd_atk = 0.0f;
+    float cooldown_ataque = 0.1;
+    float acompanhar_cd_atk = 0.0;
 
-    float cooldown_antes_de_pular = 2.0f;
-    float acompanhar_cd_pulo = 0.0f;
+    float cooldown_antes_de_pular = 2.0;
+    float acompanhar_cd_pulo = 0.0;
 
-    float velocidade_do_pulo = 1500.0f;
-    float raio_do_sapo = 40.0f;
-    float altura_do_pulo = 100.0f;
-    float duracao_do_pulo = 0.0f;
+    float cooldown_antes_do_porradao = 2.0;
+    float acompanhar_cd_porradao = 0.0;
+
+    float cooldown_antes_da_linguada = 2.0;
+    float acompanhar_cd_antes_da_linguada = 0.0;
+
+    float velocidade_do_pulo = 1500.0;
+    float raio_do_sapo = 40.0;
+    float altura_do_pulo = 100.0;
+    float duracao_do_pulo = 0.0;
 }; typedef struct Sapo sapo; sapo S;
 
 typedef struct
@@ -70,8 +81,8 @@ void init()
     dash_t = 0;
     ATK1_COOLDOWNt = 0;
 
-    P.posicao.x = rand() % 1080;
-    P.posicao.y = rand() % 720;
+    P.posicao.x = inicio_player_x;
+    P.posicao.y = inicio_player_y;
     S.posicao.x = CENTER_x;
     S.posicao.y = CENTER_y;
 
@@ -82,7 +93,7 @@ void init()
     vec.reverse = 0;
 
     S.posicao_inicial = S.posicao;
-    S.acompanhar_cd_atk == S.cooldown_ataque;
+    S.acompanhar_cd_antes_da_linguada == S.cooldown_antes_da_linguada;
     S.linguadas_restantes = S.quantidade_de_linguadas;
 }
 
@@ -93,8 +104,8 @@ void movement()
     if (dash_t > 10) dash_t++;				                //timer do dash, ele é empregado de modo que o jogador não possa ficar usando o dash indefinidamente
     if (dash_t >= 150) dash_t = 0;			                //se o dash_timer for maior que o intervalo de tempo de espera, resete ele, o jogador pode usar o dash 
 
-    if (IsKeyDown(KEY_W)) 
-    {  
+    if (IsKeyDown(KEY_W))
+    {
         if (dash_t <= 10 && IsKeyDown(KEY_LEFT_CONTROL))		//dash timer <= 50 é para que o dash seja perceptível e suave para o jogador.
         {
             P.posicao.y -= 20;
@@ -102,7 +113,7 @@ void movement()
         }
         else P.posicao.y -= 4;
     }
-    if (IsKeyDown(KEY_A)) 
+    if (IsKeyDown(KEY_A))
     {
         if (dash_t <= 10 && IsKeyDown(KEY_LEFT_CONTROL))		//dash timer <= 50 é para que o dash seja perceptível e suave para o jogador.
         {
@@ -111,7 +122,7 @@ void movement()
         }
         else P.posicao.x -= 4;
     }
-    if (IsKeyDown(KEY_S)) 
+    if (IsKeyDown(KEY_S))
     {
         if (dash_t <= 10 && IsKeyDown(KEY_LEFT_CONTROL))		//dash timer <= 50 é para que o dash seja perceptível e suave para o jogador.
         {
@@ -120,7 +131,7 @@ void movement()
         }
         else P.posicao.y += 4;
     }
-    if (IsKeyDown(KEY_D)) 
+    if (IsKeyDown(KEY_D))
     {
         if (dash_t <= 10 && IsKeyDown(KEY_LEFT_CONTROL))		//dash timer <= 50 é para que o dash seja perceptível e suave para o jogador.
         {
@@ -150,17 +161,21 @@ void vectoratk()
         float mod = 0.06 * sqrt(dx * dx + dy * dy);
         vec.cx -= dx / mod;
         vec.cy -= dy / mod;
-        if ((vec.dex > S.posicao.x && vec.dey > S.posicao.y) && (vec.cx <= S.posicao.x || vec.cy <= S.posicao.y)) { vec.reverse = 0;
-        S.linguadas_restantes--;it = -1;
+        if ((vec.dex > S.posicao.x && vec.dey > S.posicao.y) && (vec.cx <= S.posicao.x || vec.cy <= S.posicao.y)) {
+            vec.reverse = 0;
+            S.linguadas_restantes--;it = -1;
         }
-        else if ((vec.dex > S.posicao.x && vec.dey < S.posicao.y) && (vec.cx <= S.posicao.x || vec.cy >= S.posicao.y)) { vec.reverse = 0;
-        S.linguadas_restantes--;it = -1;
+        else if ((vec.dex > S.posicao.x && vec.dey < S.posicao.y) && (vec.cx <= S.posicao.x || vec.cy >= S.posicao.y)) {
+            vec.reverse = 0;
+            S.linguadas_restantes--;it = -1;
         }
-        else if ((vec.dex  < S.posicao.x && vec.dey > S.posicao.y) && (vec.cx >= S.posicao.x || vec.cy <= S.posicao.y)) { vec.reverse = 0;
-        S.linguadas_restantes--;it = -1;
+        else if ((vec.dex  < S.posicao.x && vec.dey > S.posicao.y) && (vec.cx >= S.posicao.x || vec.cy <= S.posicao.y)) {
+            vec.reverse = 0;
+            S.linguadas_restantes--;it = -1;
         }
-        else if ((vec.dex < S.posicao.x && vec.dey < S.posicao.y) && (vec.cx >= S.posicao.x || vec.cy >= S.posicao.y)) { vec.reverse = 0;
-        S.linguadas_restantes--; it = -1;
+        else if ((vec.dex < S.posicao.x && vec.dey < S.posicao.y) && (vec.cx >= S.posicao.x || vec.cy >= S.posicao.y)) {
+            vec.reverse = 0;
+            S.linguadas_restantes--; it = -1;
         }
     }
 }
@@ -170,6 +185,16 @@ void vectoratk()
 void ataques_sapo(float controle_de_tempo) //mudar isso, a função ta muito grande e deve ser modularizada de modo a facilitar a legibilidade do codigo.
 {
     //-----------------------------------------------------// LINGUADA:
+    if (S.tempo_antes_da_linguada == true) {
+        S.acompanhar_cd_antes_da_linguada -= controle_de_tempo;
+
+        if (S.acompanhar_cd_antes_da_linguada <= 0.0) {
+            S.tempo_antes_da_linguada = false;
+            S.linguada = true;
+            S.acompanhar_cd_atk = S.cooldown_ataque;
+        }
+    }
+
     if (S.linguada == true) {
         if (S.linguadas_restantes <= 0) {
             S.linguada = false;
@@ -185,11 +210,11 @@ void ataques_sapo(float controle_de_tempo) //mudar isso, a função ta muito gra
                 vec.dex = (P.posicao.x);
                 vec.dey = (P.posicao.y);
             }
-            else if (it > 50) 
+            else if (it > 50)
             {
                 if (S.posicao.y == P.posicao.y) S.posicao.y += 1;
                 if (S.posicao.x == P.posicao.x) S.posicao.x += 1;
-                vectoratk(); 
+                vectoratk();
             }
             it++;
         }
@@ -199,51 +224,63 @@ void ataques_sapo(float controle_de_tempo) //mudar isso, a função ta muito gra
     if (S.tempo_antes_de_pular == true) {
         S.acompanhar_cd_pulo -= controle_de_tempo;
 
-        if (S.acompanhar_cd_pulo <= 0.0f) {
+        if (S.acompanhar_cd_pulo <= 0.0) {
             S.tempo_antes_de_pular = false;
             S.pulando = true;
 
             S.posicao_do_pulo = P.posicao;
             S.posicao_inicial = S.posicao;
 
-            S.duracao_do_pulo = 0.0f;
+            S.duracao_do_pulo = 0.0;
         }
+
     }
 
     if (S.pulando == true) {
-        S.duracao_do_pulo += controle_de_tempo * 1.5f;
+        S.duracao_do_pulo += controle_de_tempo * 1.5;
 
-        float fator_mult = S.duracao_do_pulo / 0.7f;
+        float fator_mult = S.duracao_do_pulo / 0.7;
 
-        if (fator_mult > 1.0f) {
-            fator_mult = 1.0f;
+        if (fator_mult > 1.0) {
+            fator_mult = 1.0;
         }
 
         // EIXO X:
         S.posicao.x = S.posicao_inicial.x + (S.posicao_do_pulo.x - S.posicao_inicial.x) * fator_mult;
 
         // EIXO Y:
-        float altura = -4 * pow((fator_mult - 0.5f), 2) + 1;
+        float altura = -4 * pow((fator_mult - 0.5), 2) + 1;
 
         S.posicao.y = S.posicao_inicial.y - (altura * S.altura_do_pulo) + (S.posicao_do_pulo.y - S.posicao_inicial.y) * fator_mult;
 
-        if (fator_mult >= 1.0f) { // garantir que vai parar na posição certa
+        if (fator_mult >= 1.0) { // garantir que vai parar na posição certa
             S.posicao.y = S.posicao_do_pulo.y;
 
             S.pulando = false;
-            S.porradao = true;
+            S.tempo_antes_do_porradao = true;
+
+            S.acompanhar_cd_porradao = S.cooldown_antes_do_porradao;
         }
 
         // FAZER DANO DE QUEDA
+    }
+
+    if (S.tempo_antes_do_porradao == true) {
+        S.acompanhar_cd_porradao -= controle_de_tempo;
+
+        if (S.acompanhar_cd_porradao <= 0.0) {
+            S.tempo_antes_do_porradao = false;
+            S.porradao = true;
+        }
     }
 
     if (S.porradao == true) {
         // FAZER DANO DO PORRADÃO
 
         S.porradao = false;
-        S.linguada = true;
+        S.tempo_antes_da_linguada = true;
 
-        S.acompanhar_cd_atk = S.cooldown_ataque;
+        S.acompanhar_cd_antes_da_linguada = S.cooldown_antes_da_linguada;
 
         if (S.reduzir_linguadas == true) {
             S.quantidade_de_linguadas--;
@@ -260,6 +297,20 @@ void ataques_sapo(float controle_de_tempo) //mudar isso, a função ta muito gra
 
     //-----------------------------------------------------//
 
+}
+
+void acompanhar_cd() {
+    if (S.tempo_antes_de_pular) {
+        DrawText("PULO CARREGANDO", 20, 20, 20, WHITE);
+    }
+
+    if (S.tempo_antes_do_porradao) {
+        DrawText("PORRADÃO CARREGANDO", 20, 20, 20, WHITE);
+    }
+
+    if (S.tempo_antes_da_linguada) {
+        DrawText("LINGUADA CARREGANDO", 20, 20, 20, WHITE);
+    }
 }
 
 //-----------------------------------------------------------//
@@ -281,6 +332,9 @@ int main() {
         ClearBackground(BLACK);
         DrawCircle(P.posicao.x, P.posicao.y, P.raio_do_player, BLUE);
         DrawCircle(S.posicao.x, S.posicao.y, S.raio_do_sapo, RED);
+
+        acompanhar_cd();
+
         //
         EndDrawing();
     }
