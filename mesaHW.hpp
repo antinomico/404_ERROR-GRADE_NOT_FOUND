7b160ffb95ab7be847e277b68a20db45a04f112e
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2025-04-06 16:12:24 -0200

    chicotada

--- mesaHW.hpp
+++ mesaHW.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "raylib.h"
 
+bool collide(float x, float y);
 class MesaHW {
     public:
         MesaHW();
         ~MesaHW();
         float x, y;
-		Image img;
-		Texture2D texture;
-		Vector2 a1, b1, c1, d1;
+                Image img;
+                Texture2D texture;
+                Vector2 a1, b1, c1, d1;
     };
-    
+
