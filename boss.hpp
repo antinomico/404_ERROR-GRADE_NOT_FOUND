7b7b6c9ce6e6e505143e84ddf1121aa18eb33e06
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2025-04-06 16:14:54 -0200

    chicotada

--- boss.hpp
+++ boss.hpp
@@ -1,22 +1,36 @@
 #pragma once
 #include "raylib.h"
+#include "player.hpp"
+
+struct chicote
+{
+        int reverse;
+        float dex, dey;
+        float cx, cy;
+}; typedef struct chicote chicote;
 
 class BossSD {
 public:
-	BossSD();
-	~BossSD();
-	void DrawSD();
-	void AtaqueSD();
-	void Chicotada();
-	void ContagemChoque();
-	void ContagemMapaK();
-	Vector2 positionSD;
-	int timer;
-	int timer_choque;
-	int etapa; // 0 - FUNDO NORMAL (CHICOTADA) // 1 - CONTAGEM // 2 - CHOQUE // 3 - FUNDO SEM MESAS // 4 - MAPA K VAZIO // 5 - MAPA K PREENCHIDO
+        BossSD();
+        ~BossSD();
+        void DrawSD();
+        void AtaqueSD(Player P, chicote* vec);
+        void Chicotada(Player P, chicote* vec);
+        void Contagem();
+        void MapaK();
+        Vector2 positionSD;
+        int timer_chicotada;
+        int timer;
+        int timer_choque;
+        int etapa; // 0 - FUNDO NORMAL (CHICOTADA) // 1 - CONTAGEM // 2 - CHOQUE // 3 - FUNDO SEM MESAS // 4 - MAPA K VAZIO // 5 - MAPA K PREENCHIDO
 
 private:
-	Texture2D spriteSD;
-	float lifeBarSD = 100.00;
-	float speedSD = 0.4;
+        Texture2D spriteSD;
+        float lifeBarSD = 100.00;
+        float speedSD = 0.4;
 };
+
+
+
+
+
